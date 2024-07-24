fcc72e8a7c1747851066333f5050faff350cae08
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-24 19:54:36 +0530

    Time: 471 ms (76.28%), Space: 0B (100%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# Finding average experience years of employee for each project_id
 select project_id, round(avg(experience_years),2) as average_years
 from project pr
 inner join employee emp
