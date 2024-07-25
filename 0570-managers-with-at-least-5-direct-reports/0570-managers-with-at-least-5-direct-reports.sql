449a9ee883ef7442c4973136b87bba620c7e513a
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-25 19:15:30 +0530

    Time: 432 ms (27.34%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# First select all manager id to which more than id belongs 
 with mng_id as(
 select managerId from(
 select managerId, count(id)
@@ -7,7 +8,7 @@ where managerId is not NULL
 group by managerId
 having count(id)>=5) t1
 )
-
+# Checking for employee having id equal to manager id
 select name from employee
 inner join mng_id
 on id=mng_id.managerId
