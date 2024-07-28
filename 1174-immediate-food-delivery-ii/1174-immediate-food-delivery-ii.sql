0bb4635e1c65daca28c3a37883f3948633a65f91
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-28 22:19:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select 
+round(sum(case when order_state ='immediate' then 1 else 0 end)/count(*),2)*100 as immediate_percentage
+from(
+select delivery_id,customer_id,
+row_number() over(partition by customer_id order by order_date) as rn,
+case when order_date=customer_pref_delivery_date then "immediate"
+else "scheduled" end as order_state
+from delivery
+) del
+where rn=1
