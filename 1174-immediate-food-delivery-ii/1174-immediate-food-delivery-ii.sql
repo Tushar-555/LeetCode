b0ef1fff44ad2b55920339f30cca62e0567773b7
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-28 22:19:32 +0530

    Time: 543 ms (79.31%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select 
-round(sum(case when order_state ='immediate' then 1 else 0 end)/count(*),2)*100 as immediate_percentage
+round(sum(case when order_state ='immediate' then 1 else 0 end)*100/count(*),2) as immediate_percentage
 from(
 select delivery_id,customer_id,
 row_number() over(partition by customer_id order by order_date) as rn,
