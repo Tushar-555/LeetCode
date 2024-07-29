ad928904dcb65527abdd59675f1aed0376488e1d
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-29 23:57:24 +0530

    Time: 707 ms (30.03%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 select 
 round(sum(case when date_diff=1 then 1 else 0 end)/count(distinct player_id),2) as fraction
 from(
@@ -6,3 +7,10 @@ select player_id,
 datediff(lead(event_date,1) over(partition by player_id order by event_date),event_date) as date_diff
 from activity
 ) act
+*/
+select round(
+(select count(distinct a.player_id) from activity a
+inner join(
+select player_id,min(event_date) first_logged_date from activity group by player_id) b
+on datediff(a.event_date,b.first_logged_date)=1 and a.player_id=b.player_id)/
+(select count(distinct player_id) from activity),2) as fraction
