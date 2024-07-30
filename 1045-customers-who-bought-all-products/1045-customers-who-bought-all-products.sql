93b50300aa2c5cfd65478cb5446c0dead060b38c
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-30 22:11:28 +0530

    Time: 615 ms (33.63%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,9 +1,8 @@
 # Write your MySQL query statement below
 select customer_id from(
-select customer_id,count(*) as cnt
+select customer_id,count(distinct product_key) as cnt
 from customer cust
-inner join product prd
-on cust.product_key=prd.product_key and cust.product_key is not null
+where cust.product_key is not null
 group by customer_id
 )cust_bought_all_prd
 where cnt=(select count(distinct product_key) from product)
