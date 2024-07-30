1ee318f50d73a38479d052de27bafa08633300e7
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-30 20:59:17 +0530

    Time: 900 ms (87.63%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 select product_id,year as first_year,quantity,price
 from(
+# Can be solved using only sales table without using product table
+# First partition sales data based on product_id and then order it by their sold year asc
 select sal.product_id,year,quantity,price,
 dense_rank() over(partition by product_id order by year) rn
 from sales sal
