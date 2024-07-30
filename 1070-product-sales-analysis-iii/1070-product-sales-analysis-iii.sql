e1dac2b00abfc04239924d6f8a86703e3112cef9
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-07-30 20:56:47 +0530

    Time: 1919 ms (10.07%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 select product_id,year as first_year,quantity,price
 from(
-select prd.product_id,year,quantity,price,
+select sal.product_id,year,quantity,price,
 dense_rank() over(partition by product_id order by year) rn
 from sales sal
-inner join product prd
-on sal.product_id=prd.product_id
+#inner join product prd
+#on sal.product_id=prd.product_id
 )product_first_sold_year
 where rn=1
