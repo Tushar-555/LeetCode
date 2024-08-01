90072279198f70eda776f08d303920eab947e10e
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-08-01 21:36:53 +0530

    Time: 443 ms (68.82%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-#select product_id,
-#case when min(change_date)>2019-08-16 then 10 end as price
-#from products;
+# First part of union is to select product and their prices before given date
 select product_id,new_price as price from(
 select product_id,new_price,
 row_number() over(partition by product_id order by change_date desc) as rn
@@ -10,6 +8,7 @@ where change_date<'2019-08-17'
 )price_of_all1
 where rn=1
 union all
+# This part of union is to select product and their prices after given date
 select distinct product_id, 10 as price
 from products
 where change_date>'2019-08-16' and product_id not in (
