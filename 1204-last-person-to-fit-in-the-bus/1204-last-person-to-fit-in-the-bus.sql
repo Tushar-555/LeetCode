340bbf78f875dd59e85378e644912efdd7937469
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-08-01 23:59:08 +0530

    Time: 992 ms (46.08%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+with max_turn as(
+select max(turn) m_turn from(
+select turn,person_id,person_name,
+sum(weight) over(order by turn) as total_weight
+from queue
+)last_person
+where total_weight<1001
+)
+
+select q.person_name from queue q
+join max_turn mt
+on turn=m_turn
+#select * from queue order by turn
