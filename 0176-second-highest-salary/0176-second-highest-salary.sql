cb28d1f46d8e192fed876e9271d6e402e5cb093e
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-08-09 23:55:30 +0530

    Time: 243 ms (58.36%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+/*
 select 
 case when max(rn) <=1 then NULL
 else salary end as SecondHighestSalary 
@@ -8,3 +9,8 @@ dense_rank() over(order by salary desc) as rn
 from employee
 )emp
 where rn=2
+*/
+# or
+
+select max(salary) as SecondHighestSalary from employee e1
+where salary<(select max(salary) from employee)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
