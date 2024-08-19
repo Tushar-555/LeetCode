15bb1e9333189ec95d98d26eab59ecafb055a623
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-08-19 23:56:00 +0530

    Time: 353 ms (88.01%), Space: 70.2 MB (5.48%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.py
+++ 0584-find-customer-referee/0584-find-customer-referee.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
     result=customer[((customer.referee_id).isna()==True) | ((customer.referee_id)!=2)]
     result=result[['name']]
