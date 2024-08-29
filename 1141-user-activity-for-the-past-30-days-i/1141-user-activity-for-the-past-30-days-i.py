6e329967415ddc56904984c4fb93f9013cf25f97
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-08-30 01:11:56 +0530

    Time: 394 ms (75.18%), Space: 70.3 MB (73.24%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.py
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 def user_activity(activity: pd.DataFrame) -> pd.DataFrame:
     activity=activity[(activity['activity_date']>'2019-06-27') & (activity['activity_date']<'2019-07-28')]
-    activity1=activity[['activity_date','user_id']].copy()
+    #activity1=activity[['activity_date','user_id']].copy()
     # or 
-    # activity1=activity.filter(['activity_date','user_id])
+    activity1=activity.filter(['activity_date','user_id'])
     activity2=activity1.drop_duplicates()
     activity_count=activity2.groupby(['activity_date']).count().reset_index()
     #print(activity_count)
