0d59626a6d9a15c72f566d8394b81b4fcb2bb08a
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2024-10-31 16:33:04 +0530

    Time: 374 ms (95.22%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,20 @@
 # Write your MySQL query statement below
-select id from(
-select id,recordDate,temperature,
-LAG(temperature,1) Over(Order by  recordDate) as prevtemp,
-DATEDIFF(recordDate,LAG(recordDate,1) OVER (ORDER BY recordDate)) as date_diff
-from weather w 
+
+# In this question it is essential to consider date difference as well while checking difference between temperature
+select id from (
+select id,
+temperature,
+LAG(temperature,1) over(order by recordDate) as prev_temperature,
+DATEDIFF(recordDate,LAG(recordDate,1) over(order by recordDate)) as date_diff
+from weather
 ) wth
-where temperature>prevtemp and  date_diff=1;
+where date_diff=1 and temperature>prev_temperature
+/*
+select id from(
+select id,
+case when temperature-coalesce((LAG(temperature,1) over(order by recordDate)),temperature)>0 then '1'
+else '0' end  as status
+from weather w
+) wth
+where status='1' 
+*/
