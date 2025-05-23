a464bf96c4f49135054d3808f45efd98c0ff4a15
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2025-05-24 00:18:17 +0530

    Time: 7 ms (89.76%), Space: 17.9 MB (63.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,38 @@
+# Using Hash Table
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        count = [0]*26
+    def isAnagram(self, s: str, t:str) -> bool:
+        count = defaultdict(int)
 
         # Count the frequency of characters in string s
         for x in s:
-            count[ord(x) - ord('a')] += 1
+            count[x] += 1
 
         # Decrement the frequency of characters in string t
         for x in t:
-            count[ord(x) - ord('a')] -= 1
+            count[x] -= 1
 
         # Check if any character has non-zero frequency 
-        for val in count:
-            if  val != 0:
+        for val in count.values():
+            if val != 0:
                 return False
+        return True
+
+#  Using Hash table (using array)
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         count = [0]*26
+
+#         # Count the frequency of characters in string s
+#         for x in s:
+#             count[ord(x) - ord('a')] += 1
+
+#         # Decrement the frequency of characters in string t
+#         for x in t:
+#             count[ord(x) - ord('a')] -= 1
+
+#         # Check if any character has non-zero frequency 
+#         for val in count:
+#             if  val != 0:
+#                 return False
         
         return True
