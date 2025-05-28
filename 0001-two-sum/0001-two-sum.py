6aad4907bb97070acb3abb091904cd3c025193b8
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2025-05-29 01:05:00 +0530

    Time: 0 ms (100%), Space: 18.8 MB (62.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,20 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(0,len(nums)-1):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+
+# Using Hash table
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+        num_map = {}
+        n = len(nums)
+
+        for i in range(0, n):
+            complement = target - nums[i]
+            if complement in num_map:
+                return [num_map[complement],i]
+            num_map[nums[i]] = i
+        
+        return []
