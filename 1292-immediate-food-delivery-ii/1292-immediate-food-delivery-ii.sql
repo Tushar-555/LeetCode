98824d5c38b7973f94ec306041b02d769851bd07
Tushar-555 <76621459+Tushar-555@users.noreply.github.com>
2025-06-03 01:47:15 +0530

    Time: 638 ms (59.32%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+# solution 1 using subquery
 -- select 
 -- round(sum(case when order_state ='immediate' then 1 else 0 end)*100/count(*),2) as immediate_percentage
 -- from(
@@ -10,13 +11,24 @@
 -- ) del
 -- where rn=1
 
-WITH customer_first_order as (
-    SELECT delivery_id, order_date, customer_pref_delivery_date,
-    row_number() over(partition by customer_id order by order_date) as rn
-    FROM delivery
-)
+# solution 2 using with and window function
+-- WITH customer_first_order as (
+--     SELECT delivery_id, order_date, customer_pref_delivery_date,
+--     row_number() over(partition by customer_id order by order_date) as rn
+--     FROM delivery
+-- )
 
-SELECT 
-round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100/count(delivery_id),2) as immediate_percentage
-FROM customer_first_order
-WHERE rn = 1
+-- SELECT 
+-- round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100/count(delivery_id),2) as immediate_percentage
+-- FROM customer_first_order
+-- WHERE rn = 1
+
+# solution 3 with aggregate function
+SELECT
+round(avg(order_date = customer_pref_delivery_date)*100,2) as immediate_percentage
+FROM delivery
+WHERE (customer_id, order_date) in (
+    SELECT customer_id, min(order_date)
+    FROM delivery
+    GROUP BY customer_id
+)
